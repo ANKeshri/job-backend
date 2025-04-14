13f21b8fed5fb1824f44c95d6be8315ed7dc362d
ANKeshri <ankeshri2003@gmail.com>
2025-04-14 16:19:28 +0530

    update

--- controllers/userController.js
+++ controllers/userController.js
@@ -47,6 +47,8 @@ export const logout = catchAsyncError(async (req, res, next) => {
     res.status(201).cookie('token', null, {
         expires: new Date(Date.now()),
         httpOnly: true,
+        secure:true,
+        sameSite: "None",
     }).json({
         success: true,
         message: 'Logged out successfully',
